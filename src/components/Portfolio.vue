e354611b3c4017137873aff7ffec5124c7700658
CeciliaL1 <cecilia.lepik@hotmail.com>
2024-06-10 10:01:50 +0200

    :sparkles: adding project

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -5,7 +5,8 @@ import { Project } from '../models/Project'
 const projects = ref<(Project[])>([
     new Project('Webbshop', 'https://cecilial1.github.io/Gottfrids-Munkar/', 'A Fictiv webbshop in the course JavaScript Basics'),
     new Project('Phobia Quiz', 'https://github.com/CeciliaL1/PhobiaQuiz', 'A quiz about phobias from the course JavaScript Bascis'),
-    new Project('To the moon', 'https://cecilial1.github.io/Kundprojekt/', 'A one pager from the course HTML/CSS')
+    new Project('To the moon', 'https://cecilial1.github.io/Kundprojekt/', 'A one pager from the course HTML/CSS'),
+    new Project('Interactive rating', 'https://calm-sea-02ea92d03.5.azurestaticapps.net/', 'Small rating component made with Frontend Mentor'),
 ])
 
 </script>
